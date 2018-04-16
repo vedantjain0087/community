f29defbef6c394876663176f8694c3be9d1334b6
vedantjain0087 <mainvedant.vns123@gmail.com>
2018-04-16 19:10:18 +0530

    first commit

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Chat',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('message', models.CharField(max_length=500)),
+                ('new', models.CharField(max_length=5)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('community', models.CharField(max_length=50)),
+            ],
+        ),
+    ]
